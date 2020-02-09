ff372219fb053245442e43dc152f8b23d04f97d3
Joanna <j.stadnik123@gmail.com>
2020-02-09 14:08:51 +0000

    added handling for random responses

--- bot/handlers.py
+++ bot/handlers.py
@@ -1,13 +1,15 @@
 from telegram.ext import CommandHandler, MessageHandler, Filters, ConversationHandler
 
 TYPE, COLOR = range(2)
-YES_NO_DICT = {
-    'yes': True,
-    'no': False,
-}
+YES_REPLIES = ['yes', 'yeah', 'yup', 'Yes', 'Yeah', 'Yup']
+NO_REPLIES = ['no', 'nope', 'nah', 'No', 'Nope', 'Nah']
+LEGIT_REPLIES = YES_REPLIES + NO_REPLIES
 
 def get_reply(update):
-    return YES_NO_DICT[update.message.text.lower()]
+    reply = update.message.text
+    if reply in YES_REPLIES:
+        return True
+    return False
 
 def start(update, context):
     update.message.reply_text("Hi, you want to play a game")
@@ -41,16 +43,21 @@ def cancel(update, context):
     return ConversationHandler.END
     
 
+def unknown_message(update, context):
+    update.message.reply_text("I am not a chat bot, reply yes or no only")
+    # Return none for state to not change
+    return None
+
 def setup(updater):
     dp = updater.dispatcher
 
     conv_handler = ConversationHandler(
         entry_points = [CommandHandler('start', start)],
         states = {
-            TYPE: [MessageHandler(Filters.regex('^(yes|no)$'), process_type)], 
-            COLOR: [MessageHandler(Filters.regex('^(yes|no)$'), process_color)],
+            TYPE: [MessageHandler(Filters.text(LEGIT_REPLIES), process_type)], 
+            COLOR: [MessageHandler(Filters.text(LEGIT_REPLIES), process_color)],
         },
-        fallbacks=[CommandHandler('cancel', cancel)],
+        fallbacks=[CommandHandler('cancel', cancel), MessageHandler(Filters.text, unknown_message)],
         name="20qs",
         persistent=True
     )
