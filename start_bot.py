eb4c6433f284cc497d1eaeeccccb6d417624c7cc
Joanna <j.stadnik123@gmail.com>
2020-02-10 22:38:51 +0000

    Added option to start bot with command line token argument

--- start_bot.py
+++ start_bot.py
@@ -1,11 +1,32 @@
-from telegram.ext import (
-    Updater,
-    DictPersistence,
-)
-from bot.token import TOKEN
-from bot.handlers import setup
+from argparse import ArgumentParser
 
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--token",
+        type=str,
+        help="If not passed, the token stored in bot/token.py will be used.",
+    )
+    args = parser.parse_args()
+    token = args.token
+    if token is None:
+        try:
+            from bot.token import TOKEN
 
-per = DictPersistence()
-updater = Updater(token=TOKEN, persistence=per, use_context=True)
-setup(updater)
+            token = TOKEN
+        except ImportError:
+            print(
+                "The secret token must be specified either in the bot/token.py "
+                "file or passed as an argument"
+            )
+
+    if token:
+        from telegram.ext import (
+            Updater,
+            DictPersistence,
+        )
+        from bot.handlers import setup
+
+        per = DictPersistence()
+        updater = Updater(token=token, persistence=per, use_context=True)
+        setup(updater)
